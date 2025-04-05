20f6919be7ce3548e4be78ef962c79cdc383eb25
백지현 <racheal1227@gmail.com>
2025-04-05 21:19:21 +0900

    feat: page와 layout의 기본 구조 설계
    - 애플리케이션 페이지 구조 초기 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-
 export default async function Home() {
   return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    <main className="flex-1 flex flex-col gap-6 px-4">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem iste rerum quae dicta quasi fuga tempore
+      alias, eius obcaecati perferendis iusto, fugiat non eaque aspernatur sunt eveniet minima, minus modi.
+    </main>
+  )
 }
