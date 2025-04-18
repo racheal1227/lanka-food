1eb1d9b6f5f4c3f5168f6b3734dec09b8a117ded
백지현 <racheal1227@gmail.com>
2025-04-18 10:12:29 +0900

    🏷️ feat: supabase client 타입 지정

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { Database } from '@/types/database.types'
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
 
 export default supabase
