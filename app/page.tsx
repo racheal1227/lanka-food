396f6b9c49097c0374f4f5ee92a9ec1200fae65f
백지현 <racheal1227@gmail.com>
2025-04-22 13:05:31 +0900

    📦 chore: supabase, next 패키지 버전 명시

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 export default async function Home() {
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem iste rerum quae dicta quasi fuga tempore
       alias, eius obcaecati perferendis iusto, fugiat non eaque aspernatur sunt eveniet minima, minus modi.
     </main>
   )
