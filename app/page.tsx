14d2fd852fbe00c039a19e919e57367322792947
백지현 <racheal1227@gmail.com>
2025-04-23 21:55:30 +0900

    ✨ feat: 상품 목록 컴포넌트 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import ProductList from '@components/product-list'
+
 export default async function Home() {
   return (
-    <main className="flex-1 flex flex-col gap-6 px-4">
-      alias, eius obcaecati perferendis iusto, fugiat non eaque aspernatur sunt eveniet minima, minus modi.
-    </main>
+    <div className="flex-1 flex flex-col gap-6" style={{ border: '1px solid red' }}>
+      <ProductList />
+    </div>
   )
 }
---
+++ components/product-list.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { ImageIcon } from 'lucide-react'
+
+import { CldImage } from 'next-cloudinary'
+
+import { getProducts } from '@/services/product.service'
+import { Product } from '@/types/database.models'
+
+export default function ProductList() {
+  const { data: products } = useSuspenseQuery<Product[]>({ queryKey: ['products'], queryFn: getProducts })
+
+  if (products.length === 0) {
+    return <div>상품이 없습니다.</div>
+  }
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+      {products.map((product) => (
+        <div key={product.id} className="border rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow">
+          <div className="relative aspect-square w-full">
+            {product.featured_image ? (
+              <CldImage
+                width="400"
+                height="400"
+                src={product.featured_image}
+                alt={product.name_ko}
+                crop="fill"
+                gravity="center"
+                loading="lazy"
+                className="w-full h-full object-cover"
+              />
+            ) : (
+              <div className="bg-gray-200 h-full w-full flex items-center justify-center">
+                <ImageIcon className="w-12 h-12 text-gray-300" />
+              </div>
+            )}
+          </div>
+          <div className="p-4">
+            <h3 className="font-semibold">{product.name_ko}</h3>
+            <p className="text-gray-600 mt-1 text-sm line-clamp-2">{product.description}</p>
+            <div className="mt-2 text-right font-semibold">{product.price_krw.toLocaleString()} 원</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
