32b8ce8c9010280e615de0c4d07c2e2d24d71f0a
백지현 <racheal1227@gmail.com>
2025-04-23 20:31:06 +0900

    ✨ feat: Provider 컴포넌트 구현 (Toast, Theme, QueryClient)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import './globals.css'
 import { Geist } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 
-import MainNavigation from '@/components/navigation'
+import Providers from '@/app/providers'
+import MainNavigation from '@components/navigation'
 
 const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'
 
@@ -26,16 +27,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="bg-background text-foreground min-h-screen">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <MainNavigation />
-          <main className="flex flex-col items-center">
-            <div className="flex flex-col gap-20 max-w-5xl p-5 mt-16">{children}</div>
-            <footer className="w-full flex items-center justify-center border-t text-center text-xs gap-8 py-16">
-              <p>TODO: Footer</p>
-            </footer>
-          </main>
-        </ThemeProvider>
+      <body className="bg-background text-foreground h-screen overflow-x-hidden">
+        <Providers>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <div className="flex flex-col h-screen">
+              <MainNavigation />
+              <main className="flex-grow flex flex-col items-center p-5 pt-20">{children}</main>
+              <footer className="w-full flex items-center justify-center border-t text-center text-xs gap-8 py-8">
+                <p>TODO: Footer</p>
+              </footer>
+            </div>
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   )
---
+++ app/providers.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import * as React from 'react'
+
+import { ThemeProvider } from 'next-themes'
+
+import { Toaster } from '@/components/ui/toaster'
+
+export default function Providers({ children }: Readonly<{ children: React.ReactNode }>) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false, throwOnError: true } },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
+}
